fd2dd5bd542a230964c23ad592407cd724bb5125
Francois-Regis LANCIEN <francois-regis.lancien@niji.fr>
2018-07-31 17:45:22 +0200

    Added elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,29 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public mapStylerOptions = [
-    {
-      'featureType': 'poi',
-      'stylers': [{'visibility': 'off'}]
-    },
-    {
-      'featureType': 'transit',
-      'stylers': [{'visibility': 'off'}]
+
+    public isMapViewActivated = false;
+    public mapStylerOptions = [
+        {
+            'featureType': 'poi',
+            'stylers': [{'visibility': 'off'}]
+        },
+        {
+            'featureType': 'transit',
+            'stylers': [{'visibility': 'off'}]
+        }
+    ];
+
+    constructor() {
+        if (navigator.geolocation) {
+            navigator.geolocation.watchPosition(position => {
+                console.log(position);
+            });
+        }
     }
-  ];
 }
